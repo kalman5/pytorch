17251289f74b9c8b0d8e2b24755b912663cddc8a
xinan.lin <xinan.lin@intel.com>
2025-12-02 23:49:58 +0000

    [xpu][feature] Enable static triton kernel launcher for XPU backend.
    ghstack-source-id: 384d4cb1a05f3e64997070b0a4251ddf641db64d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/168952

--- torch/csrc/Module.cpp
+++ torch/csrc/Module.cpp
@@ -125,6 +125,10 @@
 #endif
 #endif
 
+#if defined(USE_XPU) && !defined(_WIN32)
+#include <torch/csrc/inductor/static_launcher/xpu/static_xpu_launcher.h>
+#endif
+
 #ifdef USE_DISTRIBUTED
 #ifdef USE_C10D
 #include <torch/csrc/distributed/autograd/python_autograd.h>
@@ -2152,6 +2156,9 @@ PyObject* initModule() {
 #endif
 #if defined(USE_CUDA) && !defined(USE_ROCM)
   ASSERT_TRUE(StaticCudaLauncher_init(module));
+#endif
+#if defined(USE_XPU) && !defined(_WIN32)
+  ASSERT_TRUE(StaticXpuLauncher_init(module));
 #endif
 #ifdef USE_MPS
   torch::mps::initModule(module);
